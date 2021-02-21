1b993fcbbe41974f22eb8ede749a9950daf31d58
deimantas Jakovlevas <deimantas@zappar.com>
2021-02-21 05:51:15 +0000

    add options

--- src/mse.ts
+++ src/mse.ts
@@ -1,12 +1,14 @@
-const meanSquaredError = (
-  yTrue : number[],
-  yPred : number[],
+interface Options {
   // eslint-disable-next-line no-unused-vars
-  hypothesis? : (n: number) => number,
-  step = 1,
-) => {
+  hypothesis? : (_n: number) => number,
+  step? : 1,
+}
+const meanSquaredError = (yTrue : number[], yPred : number[], options : Options) => {
+  const { step, hypothesis } = options;
   let totalSquaredError = 0;
+
   const m = yTrue.length;
+
   for (let i = 0; i < m; i += step) {
     const theta = hypothesis ? hypothesis(yTrue[i]) : yTrue[i];
     const squaredError = (theta - yPred[i]) ** 2;
